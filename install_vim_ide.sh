c89e7d7bfd9871d2d50b28f6f05562c7149a3c1c
Srijan <srijan@pitstoprecharge.com>
2013-09-18 12:20:29 +0545

    Added scripts to show git branch and dirty branch in shell prompt

--- install_vim_ide.sh
+++ install_vim_ide.sh
@@ -8,11 +8,14 @@ function vim_ide_packages
     apt-get install -y vim-gnome ruby-dev bash-completion 
     rm -rf /usr/bin/ctags
     rm -rf /usr/bin/etags
-    apt-get install -y exuberant-ctags
+    sleep 20
     pip install dbgp vim-debug
     install-vim-debug.py
     cp -r ./* $HOME/.vim
     cp -r ./.vimrc $HOME
+    apt-get install -y exuberant-ctags
+    sed -i "$ aexport GIT_PS1_SHOWDIRTYSTATE=1" >> $HOME/.bashrc
+    sed -i "$ aexport PS1='\[\033[0;34m\]\u@\w\[\033[0;32m\]$(__git_ps1)\[\033[0;34m\]\$\[\033[0m\] '" >> $HOME/.bashrc
    
 }
 
